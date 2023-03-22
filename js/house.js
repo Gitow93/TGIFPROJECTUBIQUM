45af24bca41b615138f05dfffe22d65a0b769606
Jorge <jorge.jimenezmunoz.93@gmail.com>
2023-03-22 18:05:17 +0100

    modified somo files of the project

--- js/house.js
+++ js/house.js
@@ -1,17 +1,16 @@
-import {houseData} from "/json/house-data.mjs"
-import {states} from "/json/states-data.mjs"
+import { houseData } from "/json/house-data.mjs";
+import { states } from "/json/states-data.mjs";
 
 const members = houseData.results[0].members;
-const checkboxes = document.querySelectorAll("input[type=checkbox]"); 
-const dropdownMenu = document.querySelector('.states-options'); 
-let check = [] 
+const checkboxes = document.querySelectorAll("input[type=checkbox]");
+const dropdownMenu = document.querySelector(".states-options");
+let check = [];
 
-function createTable(array) { 
+function createTable(array) {
+  const tableBody = document.querySelector(".table_body");
+  tableBody.innerHTML = "";
 
-const tableBody = document.querySelector(".table_body"); 
-tableBody.innerHTML = ''; 
-
-for (let i = 0; i < array.length; i++) { 
+  for (let i = 0; i < array.length; i++) {
     const name = array[i].first_name + " " + array[i].last_name;
     const url = array[i].url;
     const party = array[i].party;
@@ -19,82 +18,83 @@ for (let i = 0; i < array.length; i++) {
     const yearsInOffice = array[i].seniority;
     const votesPercentage = array[i].votes_with_party_pct;
 
-    const tableRow = document.createElement("tr"); 
-    const link = document.createElement("a")
+    const tableRow = document.createElement("tr");
+    const link = document.createElement("a");
 
     link.href = url;
-    link.appendChild(document.createTextNode(name)); 
+    link.appendChild(document.createTextNode(name));
 
     const nameCell = document.createElement("td");
     const partyCell = document.createElement("td");
     const stateCell = document.createElement("td");
-    const yearsInOfficeCell = document.createElement("td"); 
+    const yearsInOfficeCell = document.createElement("td");
     const votesPercentageCell = document.createElement("td");
 
-    nameCell.appendChild(link); 
+    nameCell.appendChild(link);
     partyCell.appendChild(document.createTextNode(party));
     stateCell.appendChild(document.createTextNode(state));
     yearsInOfficeCell.appendChild(document.createTextNode(yearsInOffice));
-    votesPercentageCell.appendChild(document.createTextNode(votesPercentage)); 
+    votesPercentageCell.appendChild(document.createTextNode(votesPercentage));
 
     tableRow.appendChild(nameCell);
     tableRow.appendChild(partyCell);
     tableRow.appendChild(stateCell);
     tableRow.appendChild(yearsInOfficeCell);
-    tableRow.appendChild(votesPercentageCell); 
+    tableRow.appendChild(votesPercentageCell);
 
-    tableBody.appendChild(tableRow); 
-    }
+    tableBody.appendChild(tableRow);
+  }
 }
 
 createTable(members);
 
-function createDropdown(array) { 
-    const states = Object.keys(array);  
-    states.forEach((key, index) => { 
-      const stateOptions = document.createElement('option'); 
-      stateOptions.value = `${key}`;
-      stateOptions.text =`${key}`; 
-      dropdownMenu.appendChild(stateOptions);                                      
-    });                                      
+function createDropdown(array) {
+  const states = Object.keys(array);
+  states.forEach((key, index) => {
+    const stateOptions = document.createElement("option");
+    stateOptions.value = `${key}`;
+    stateOptions.text = `${key}`;
+    dropdownMenu.appendChild(stateOptions);
+  });
+}
+
+createDropdown(states);
+
+function filterOptions(event) {
+  check = Array.from(checkboxes)
+    .filter((element) => element.checked)
+    .map((element) => element.value);
+  const state = dropdownMenu.value;
+  let membersFiltered = [];
+  if (check.length === 0 && state === "All states") {
+    membersFiltered = event;
+  } else {
+    event.forEach((element) => {
+      if (check.length !== 0 && state === "All states") {
+        if (check.includes(element.party)) {
+          membersFiltered.push(element);
+        }
+      } else if (check.length === 0 && state !== "All states") {
+        if (element.state === state) {
+          membersFiltered.push(element);
+        }
+      } else {
+        if (check.includes(element.party) && element.state === state) {
+          membersFiltered.push(element);
+        }
+      }
+    });
   }
-  
-createDropdown(states); 
+  return createTable(membersFiltered);
+}
 
-function filterOptions (event) { 
-check =  Array.from(checkboxes).filter(element => element.checked).map(element => element.value);
-const state = dropdownMenu.value; 
-let  membersFiltered = [];
-    if (check.length === 0 && state === "All states") {
-      membersFiltered = event; 
-    } else { 
-      event.forEach(element => {
-        if (check.length !== 0 && state === "All states") { 
-          if (check.includes(element.party)) {              
-           membersFiltered.push(element);                   
-          }
-        } else if (check.length === 0 && state !== "All states") {
-          if (element.state === state) {
-           membersFiltered.push(element);
-          }
-        } else {
-          if (check.includes(element.party) && element.state === state) { 
-            membersFiltered.push(element);                                
-          }                                                               
-        } 
-      });
-    } 
-    return createTable(membersFiltered); 
-};
+filterOptions(members);
 
-filterOptions(members)
-
-
-dropdownMenu.addEventListener('change',()=>{ 
-return filterOptions(members)
+dropdownMenu.addEventListener("change", () => {
+  return filterOptions(members);
 });
-checkboxes.forEach(function(checkbox) { 
-checkbox.addEventListener('change', () =>{
-return filterOptions(members); 
+checkboxes.forEach(function (checkbox) {
+  checkbox.addEventListener("change", () => {
+    return filterOptions(members);
+  });
 });
-})
--- js/senate.js
+++ js/senate.js
@@ -1,18 +1,20 @@
-import {senateData} from "/json/senate-data.mjs"
-import {states} from "/json/states-data.mjs"
+import { senateData } from "/json/senate-data.mjs";
+import { states } from "/json/states-data.mjs";
 
 const members = senateData.results[0].members; // creamos una variable donde le damos el valor de la ruta que nos lleva a la array con la que vamos a trabajar
 const checkboxes = document.querySelectorAll("input[type=checkbox]"); // creamos una variable que contenga los checkboxes que hemos creado en html
 //al ser con un 'querySelector' no sale una array, si no una NodeList, por lo que habrá que meterlos en una array despues
-const dropdownMenu = document.querySelector('.states-options'); // variable que contiene la clase del elemento select de nuestro html
-let check = [] // declaramos una variable que contenga una array vacía para utilizarla más tarde con nuestro filter
+const dropdownMenu = document.querySelector(".states-options"); // variable que contiene la clase del elemento select de nuestro html
+let check = []; // declaramos una variable que contenga una array vacía para utilizarla más tarde con nuestro filter
 
-function createTable(array) { //creamos una array para crear una tabla
+function createTable(array) {
+  //creamos una array para crear una tabla
 
-const tableBody = document.querySelector(".table_body"); // seleccionamos la clase del body de la tabla
-tableBody.innerHTML = ''; // le vaciamos el contenido
+  const tableBody = document.querySelector(".table_body"); // seleccionamos la clase del body de la tabla
+  tableBody.innerHTML = ""; // le vaciamos el contenido
 
-for (let i = 0; i < array.length; i++) { //hacemos un bucle para recorrer cada elemento de la array
+  for (let i = 0; i < array.length; i++) {
+    //hacemos un bucle para recorrer cada elemento de la array
     const name = array[i].first_name + " " + array[i].last_name; // cogemos cada propiedad con la que vayamos a trabajar en la tabla y la metemos en una variable
     const url = array[i].url;
     const party = array[i].party;
@@ -21,7 +23,7 @@ for (let i = 0; i < array.length; i++) { //hacemos un bucle para recorrer cada e
     const votesPercentage = array[i].votes_with_party_pct;
 
     const tableRow = document.createElement("tr"); // creamos el elemento 'fila' dentro de una variable
-    const link = document.createElement("a") // creamos un elemento con la etiqueta 'a' para los links de los nombres 
+    const link = document.createElement("a"); // creamos un elemento con la etiqueta 'a' para los links de los nombres
 
     link.href = url; // le damos un valor a href del link que hemos creado anteriormente
     link.appendChild(document.createTextNode(name)); // metemos la variable name dentro de la variable link
@@ -29,83 +31,91 @@ for (let i = 0; i < array.length; i++) { //hacemos un bucle para recorrer cada e
     const nameCell = document.createElement("td"); // creamos las celdas donde irán nuestras variables
     const partyCell = document.createElement("td");
     const stateCell = document.createElement("td");
-    const yearsInOfficeCell = document.createElement("td"); 
+    const yearsInOfficeCell = document.createElement("td");
     const votesPercentageCell = document.createElement("td");
 
     nameCell.appendChild(link); // metemos cada variable con las propiedades de la array dentro de cada celda creada anteriormente
     partyCell.appendChild(document.createTextNode(party));
     stateCell.appendChild(document.createTextNode(state));
     yearsInOfficeCell.appendChild(document.createTextNode(yearsInOffice));
-    votesPercentageCell.appendChild(document.createTextNode(votesPercentage)); 
+    votesPercentageCell.appendChild(document.createTextNode(votesPercentage));
 
-    tableRow.appendChild(nameCell); // metemos las celdas dentro de las filas 
+    tableRow.appendChild(nameCell); // metemos las celdas dentro de las filas
     tableRow.appendChild(partyCell);
     tableRow.appendChild(stateCell);
     tableRow.appendChild(yearsInOfficeCell);
-    tableRow.appendChild(votesPercentageCell); 
+    tableRow.appendChild(votesPercentageCell);
 
     tableBody.appendChild(tableRow); // metemos las filas dentro del table body
-}
-
+  }
 }
 
 createTable(members); // llamamos a la función
 
-function createDropdown(array) { // creamos una función para crear y poner las opciones del Dropdown
-  const states = Object.keys(array);  // con esta constante cojemos solo la propiedad de 'key' del Objeto, obviando la de 'value' (mirar referencia si hace falta)
-  states.forEach((key, index) => { // hacemos un 'forEach.' para que para cada uno de nuestros elementos 'KEY', haga lo siguiente:
-    const stateOptions = document.createElement('option'); // se cree un elemento option
-    stateOptions.value = `${key}`;// donde el valor será la key del objeto que hemos cogido antes
-    stateOptions.text =`${key}`; // así como también lo será el texto que nos aparezca
-    dropdownMenu.appendChild(stateOptions); // finalmente añadimos las opciones a nuestra variable 'dropdownMenu' que creamos antes                                     
-  });                                      // cuyo valor es el elemento con clase 'state-options'
+function createDropdown(array) {
+  // creamos una función para crear y poner las opciones del Dropdown
+  const states = Object.keys(array); // con esta constante cojemos solo la propiedad de 'key' del Objeto, obviando la de 'value' (mirar referencia si hace falta)
+  states.forEach((key, index) => {
+    // hacemos un 'forEach.' para que para cada uno de nuestros elementos 'KEY', haga lo siguiente:
+    const stateOptions = document.createElement("option"); // se cree un elemento option
+    stateOptions.value = `${key}`; // donde el valor será la key del objeto que hemos cogido antes
+    stateOptions.text = `${key}`; // así como también lo será el texto que nos aparezca
+    dropdownMenu.appendChild(stateOptions); // finalmente añadimos las opciones a nuestra variable 'dropdownMenu' que creamos antes
+  }); // cuyo valor es el elemento con clase 'state-options'
 }
 
 createDropdown(states);
 
+function filterOptions(event) {
+  //creamos una función donde pondremos el evento que deseamos filtrar (a los eventos siempre se les llama EVENT o EV!!)
+  check = Array.from(checkboxes)
+    .filter((element) => element.checked)
+    .map((element) => element.value); // igualamos una constate state al 'value' de los checkboxes
+  // la variable de array 'check' que declaramos al principio la convertimos en un array de los checkboxes,
+  // a los que le hacemos un filter con los elementos checked y luego lo convertimos en una nueva array con map de dichos elementos
+  const state = dropdownMenu.value; // creamos una variable con el 'value' del elemento select del html que será nuestro dropdown (donde filtraremos los estados)
+  let membersFiltered = []; // declaramos una variable array que después nos servirá para 'pushear' nuestros elementos 'filtrados'
+  if (check.length === 0 && state === "All states") {
+    membersFiltered = event; // damos la primera condición (la condición por defecto) donde mostramos todo
+  } else {
+    // ahora hacemos una condición para cada caso que queramos
+    event.forEach((element) => {
+      if (check.length !== 0 && state === "All states") {
+        // en esta primera opción aparecerá la selección 'todos los estados' pero se filtrará por el partido
+        if (check.includes(element.party)) {
+          // así pues le decimos que si nuestra array check incluye la propiedad party que queremos
+          membersFiltered.push(element); // nos la puseé en la array antes creada y nos la muestre en pantalla
+        }
+      } else if (check.length === 0 && state !== "All states") {
+        // en esta segunda opción hacemos lo mismo que en la primera pero con la propiedad de state
+        if (element.state === state) {
+          membersFiltered.push(element);
+        }
+      } else {
+        if (check.includes(element.party) && element.state === state) {
+          // en nuestra última condición le pedimos que si incluye la propiedad 'party' que queremos
+          membersFiltered.push(element); // así como que la propiedad de 'state' coincida con el 'value' del dropdown que metimos antes en la constante 'state'
+        } // nos puseé ambos elementos en la array creada y nos los devuelva
+      }
+    });
+  }
+  return createTable(membersFiltered); // finalmente nuestra array se mostrará en nuestra función donde creamos la tabla
+}
 
-function filterOptions (event) {  //creamos una función donde pondremos el evento que deseamos filtrar (a los eventos siempre se les llama EVENT o EV!!)
-    check =  Array.from(checkboxes).filter(element => element.checked).map(element => element.value); // igualamos una constate state al 'value' de los checkboxes
-      // la variable de array 'check' que declaramos al principio la convertimos en un array de los checkboxes, 
-      // a los que le hacemos un filter con los elementos checked y luego lo convertimos en una nueva array con map de dichos elementos
-    const state = dropdownMenu.value; // creamos una variable con el 'value' del elemento select del html que será nuestro dropdown (donde filtraremos los estados)
-    let  membersFiltered = []; // declaramos una variable array que después nos servirá para 'pushear' nuestros elementos 'filtrados'
-    if (check.length === 0 && state === "All states") {
-      membersFiltered = event; // damos la primera condición (la condición por defecto) donde mostramos todo
-    } else { // ahora hacemos una condición para cada caso que queramos
-      event.forEach(element => {
-        if (check.length !== 0 && state === "All states") { // en esta primera opción aparecerá la selección 'todos los estados' pero se filtrará por el partido
-          if (check.includes(element.party)) {              // así pues le decimos que si nuestra array check incluye la propiedad party que queremos
-           membersFiltered.push(element);                   // nos la puseé en la array antes creada y nos la muestre en pantalla 
-          }
-        } else if (check.length === 0 && state !== "All states") { // en esta segunda opción hacemos lo mismo que en la primera pero con la propiedad de state
-          if (element.state === state) {
-           membersFiltered.push(element);
-          }
-        } else {
-          if (check.includes(element.party) && element.state === state) { // en nuestra última condición le pedimos que si incluye la propiedad 'party' que queremos 
-            membersFiltered.push(element);                                // así como que la propiedad de 'state' coincida con el 'value' del dropdown que metimos antes en la constante 'state'
-          }                                                               // nos puseé ambos elementos en la array creada y nos los devuelva
-        } 
-      });
-    } 
-      return createTable(membersFiltered); // finalmente nuestra array se mostrará en nuestra función donde creamos la tabla
-  };
-
-  filterOptions(members) // llamamos a la función
+filterOptions(members); // llamamos a la función
 
 // function createEventListener (array) { // creamos una función para el eventlistener donde abarcaremos ambos casos
-    dropdownMenu.addEventListener('change',()=>{ // en primer lugar el dropdown, donde nos tendrá que devolver la función de 'filterOptions'
-    return filterOptions(members)
-    });
-    checkboxes.forEach(function(checkbox) { // en segundo lugar los checkboxes, donde utilizaremos el método 'forEach' para recorrer cada elemento de la array
-      checkbox.addEventListener('change', () =>{ // en cada checkbox o elemento de la array añadimos un eventlistener
-        return filterOptions(members); // le pedimos  que nos devuelva la función de 'filterOptions'
-        });
-      })
-  //}
+dropdownMenu.addEventListener("change", () => {
+  // en primer lugar el dropdown, donde nos tendrá que devolver la función de 'filterOptions'
+  return filterOptions(members);
+});
+checkboxes.forEach(function (checkbox) {
+  // en segundo lugar los checkboxes, donde utilizaremos el método 'forEach' para recorrer cada elemento de la array
+  checkbox.addEventListener("change", () => {
+    // en cada checkbox o elemento de la array añadimos un eventlistener
+    return filterOptions(members); // le pedimos  que nos devuelva la función de 'filterOptions'
+  });
+});
+//}
 
- // createEventListener(members) // llamamos a la función
-
-
-
+// createEventListener(members) // llamamos a la función
