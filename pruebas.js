a63ab5e878aaa7b25eb6e56238c22c79c5c53327
Jorge <jorge.jimenezmunoz.93@gmail.com>
2023-04-03 11:59:31 +0200

    some files have been modified

--- pruebas.js
+++ pruebas.js
@@ -1,8 +1,18 @@
-if (tableBody.innerHTML === "") {
+/* if (tableBody.innerHTML === "") {
 
   const result = document.createElement("p");
   result.setAttribute("class", "color");
   result.innerHTML = "Sorry. No results found";
   result.style.color = "red";
 
-  tableBody.appendChild(result); }
+  tableBody.appendChild(result); } */
+
+async function getUsers() {
+  let url = "users.json";
+  try {
+    let res = await fetch(url);
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
